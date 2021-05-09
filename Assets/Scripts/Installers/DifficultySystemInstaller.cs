dce879f4c2dbaa7dce29801bd45f8769536fd85d
Karol Świerczek <karolswierczek5@gmail.com>
2021-05-09 23:49:34 +0200

    feat(difficulty): add installer for difficulty system

--- Assets/Scripts/Installers/DifficultySystemInstaller.cs
+++ Assets/Scripts/Installers/DifficultySystemInstaller.cs
@@ -1,7 +1,17 @@
-﻿namespace Installers
+﻿using Data;
+using Difficulty;
+using UnityEngine;
+using Zenject;
+
+namespace Installers
 {
-    public class DifficultySystemInstaller
+    public class DifficultySystemInstaller : MonoInstaller
     {
+        [SerializeField] private DifficultySettings _Settings;
         
+        public override void InstallBindings()
+        {
+            Container.BindInterfacesTo(typeof(DifficultySystem)).AsSingle().WithArguments(_Settings);
+        }
     }
 }
